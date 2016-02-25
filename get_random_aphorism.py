d90b0fa39eda18ce4e8b0dd26f7e873363fc1218
Tomás Fartaria <fartaria.tomas@gmail.com>
2016-02-25 17:14:21 +0000

    works for linux
    cleaner code and changed to work on linux

--- get_random_aphorism.py
+++ get_random_aphorism.py
@@ -2,23 +2,29 @@ import os
 import sys
 import random
 
-def book_to_aphorisms(book_path):
+
+def text_file_to_list():
+    current_dir = os.getcwd()
+    book_path = current_dir + str(r'/aphorisms.txt')
+    print(book_path)
     with open(book_path, 'r', encoding='latin1') as book:
-        aphorisms_list = [line.strip('-\n') for line in book if len(line)>5]
-    #print(aphorisms_list)
+        aphorisms_list = [line.strip('-\n') for line in book if len(line) > 5]
+    # print(aphorisms_list)
     return aphorisms_list
 
-def main():
-    book_path = str(
-        r'C:/Users/tomas/Documents/MEGA/Python_projects/aphorisms/aphorisms.txt')
-    aphorisms_list = book_to_aphorisms(book_path)
+
+def print_random_quote(aphorisms_list):
     quote = 'a'
     while quote.encode("ascii", "ignore").decode("ascii") == quote:
-        quote = aphorisms_list[random.randint(0,len(aphorisms_list))]
+        quote = aphorisms_list[random.randint(0, len(aphorisms_list))]
         if quote.encode("ascii", "ignore").decode("ascii") == quote:
             print(quote)
             break
-        else:
-            quote = 'a'
+
+
+def main():
+    aphorisms_list = text_file_to_list()
+    print_random_quote(aphorisms_list)
+
 if __name__ == "__main__":
     main()
